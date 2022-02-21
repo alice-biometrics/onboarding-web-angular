9c881eecb1a4702feb270c0cba4b3e309ccabb9c
Lara Refojos <lrefojos@alicebiometrics.com>
2022-02-21 15:59:39 +0100

    feat(*): add token validation. Move components

--- src/app/components/token/token.component.ts
+++ src/app/components/token/token.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { isValidToken } from '../../utils/jsonWebTokens';
 @Component({
   selector: 'token',
   templateUrl: './token.component.html',
@@ -8,8 +8,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class TokenComponent {
   @Output() trialTokenEvent = new EventEmitter<string>();
   trialToken: string;
+  errorTokenMsg: boolean;
 
   getToken() {
+    if (!isValidToken(this.trialToken)) {
+      this.errorTokenMsg = true;
+      return;
+    }
     this.trialTokenEvent.emit(this.trialToken);
     this.trialToken = '';
   }
